fc66c1d5212f6bc7d5f1f014a28f98d2d4abb1cc
DESKTOP-MANSBU3\Admin <kenjiwinness@gmail.com>
2023-01-16 21:46:35 +0700

    add language choices

--- ui.py
+++ ui.py
@@ -2,10 +2,14 @@ import gradio as gr
 import whisper
 import torch
 from yt_whisper.cli import get_audio
+from whisper.tokenizer import TO_LANGUAGE_CODE
 
 default_model = "base"
 model_choices = ["tiny", "base", "small", "medium", "large"]
 label_model_size = "Model Size"
+default_language = "Auto"
+language_choices = [default_language] + sorted([k.title() for k in TO_LANGUAGE_CODE.keys()])
+label_language = "Language"
 transcribe_button_text = "Transcribe"
 transcribed_output_text = "Transcription Output"
 
@@ -15,20 +19,22 @@ def check_cuda():
     print(torch.cuda.get_device_name(0))
 
 
-def file_to_text(tmp_file_name, model_size):
+def file_to_text(tmp_file_name, model_size, language):
     check_cuda()
+    language = None if language == default_language else language
     model = whisper.load_model(model_size)
-    result = model.transcribe(tmp_file_name.name)
+    result = model.transcribe(tmp_file_name.name, language=language)
 
     return result['text']
 
 
-def youtube_to_text(url, model_size):
+def youtube_to_text(url, model_size, language):
     check_cuda()
+    language = None if language == default_language else language
     model = whisper.load_model(model_size)
     audios = get_audio([url])
     for _, audio_path in audios.items():
-        result = model.transcribe(audio_path)
+        result = model.transcribe(audio_path, language=language)
 
     return result['text']
 
@@ -38,8 +44,9 @@ with gr.Blocks() as demo:
         youtube_button = gr.Button(transcribe_button_text)
         youtube_button.click(youtube_to_text,
                                 inputs=[
-                                    gr.inputs.Textbox(label="Youtube URL"),
-                                    gr.inputs.Dropdown(choices=model_choices, label=label_model_size, default=default_model)
+                                    gr.inputs.Textbox(label="YouTube URL"),
+                                    gr.inputs.Dropdown(choices=model_choices, label=label_model_size, default=default_model),
+                                    gr.inputs.Dropdown(choices=language_choices, label=label_language, default=default_language)
                             ], outputs=gr.Textbox(label=transcribed_output_text))
 
     with gr.Tab("File"):
@@ -47,7 +54,8 @@ with gr.Blocks() as demo:
         file_button.click(file_to_text,
                             inputs=[
                                 gr.inputs.File(label="Audio File"),
-                                gr.inputs.Dropdown(choices=model_choices, label=label_model_size, default=default_model)
+                                gr.inputs.Dropdown(choices=model_choices, label=label_language, default=default_model),
+                                gr.inputs.Dropdown(choices=language_choices, label=label_language, default=default_language)
                             ],
                             outputs=gr.Textbox(label=transcribed_output_text))
 demo.launch()
